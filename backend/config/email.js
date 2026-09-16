baba789524cd7524e3abce50bf03983d38157936
Mohammad Tousif Sunkat <tousifwebwork@gmail.com>
2026-09-16 18:32:02 +0530

    nodemailer

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,11 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
   },
+
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
